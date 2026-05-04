7c22374363c5795947a7b7d7c961fb9c2558aee2
Nicolas Pasche <nicolas.pasche@proton.me>
2026-05-04 22:01:24 +0200

    Added Anthithetic variables in monte_carlo_engine.cpp, Standard error has been decreased massively, especially in linear payoffs

--- engines/monte_carlo_engine.cpp
+++ engines/monte_carlo_engine.cpp
@@ -35,16 +35,32 @@ struct MonteCarloEngine::MCResult MonteCarloEngine::price(
     double M2 = 0.0;
     int n = 0;
 
-    for (int i = 0; i < num_simulations; ++i) {
-        double ST = simulate_terminal_price();
+        for (int i = 0; i < num_simulations / 2; ++i) {
 
-        double payoff = derivative.payoff(ST);
-        double discounted_payoff = std::exp(-r * T) * payoff;
+        double Z = rng.get_normal_vector()[0];
 
+        // Pfad 1 (Z)
+        double ST1 = S0 * std::exp(
+            (r - d - 0.5 * sigma * sigma) * T 
+            + sigma * std::sqrt(T) * Z
+        );
+
+        // Pfad 2 (-Z)
+        double ST2 = S0 * std::exp(
+            (r - d - 0.5 * sigma * sigma) * T 
+            - sigma * std::sqrt(T) * Z
+        );
+
+        double payoff1 = derivative.payoff(ST1);
+        double payoff2 = derivative.payoff(ST2);
+
+        double discounted_avg = std::exp(-r * T) * 0.5 * (payoff1 + payoff2);
+
+        // Welford Update
         n++;
-        double delta = discounted_payoff - mean;
+        double delta = discounted_avg - mean;
         mean += delta / n;
-        double delta2 = discounted_payoff - mean;
+        double delta2 = discounted_avg - mean;
         M2 += delta * delta2;
     }
 
@@ -52,4 +68,4 @@ struct MonteCarloEngine::MCResult MonteCarloEngine::price(
     double standard_error = std::sqrt(variance / n);
 
     return {mean, standard_error};
-} 
+}
